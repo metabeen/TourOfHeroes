9de2792a839acad178a69ccbbfe58683ae0701e5
Metabeen <cyril.deschamps88@gmail.com>
2017-04-26 16:19:47 +0200

    Update premier jour angular js2

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -6,17 +6,24 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var core_1 = require("@angular/core");
+var hero_1 = require("./hero");
 var AppComponent = (function () {
     function AppComponent() {
-        this.title = 'Tour of Heroes';
-        this.hero = 'Windstorm';
+        this.title = 'Tour of heroes';
+        this.heroes = [
+            new hero_1.Hero(1, 'Magneto'),
+            new hero_1.Hero(13, 'Wolverine'),
+            new hero_1.Hero(15, 'Mystic'),
+            new hero_1.Hero(20, 'Professor X')
+        ];
+        this.myHero = this.heroes[0];
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>{{title}}</h1><h2>{{hero}}'s caracteristics</h2>",
+        template: "\n<h1>{{title}}</h1>\n<h2>My favorite hero is: {{myHero.name}}</h2>\n<p>Heroes:</p>\n\t<ul>\n\t\t<li *ngFor=\"let hero of heroes\">\n\t\t{{hero.name}}\n\t\t</li>\n\t</ul>\n"
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero'
 
 @Component({
   selector: 'my-app',
-template: `<h1>{{title}}</h1><h2>{{hero}}'s caracteristics</h2>`,
+template: `
+<h1>{{title}}</h1>
+<h2>My favorite hero is: {{myHero.name}}</h2>
+<p>Heroes:</p>
+	<ul>
+		<li *ngFor="let hero of heroes">
+		{{hero.name}}
+		</li>
+	</ul>
+`
 })
 export class AppComponent  { 
-title = 'Tour of Heroes'; 
-hero = 'Windstorm';
+title = 'Tour of heroes'; 
+heroes = [
+new Hero(1, 'Magneto'),
+new Hero(13, 'Wolverine'),
+new Hero(15, 'Mystic'),
+new Hero(20, 'Professor X')
+];
+myHero = this.heroes[0];
 }
